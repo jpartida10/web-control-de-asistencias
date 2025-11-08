6e1be346f5ff35267dd2084bd6b036202c9527fe
jpartida10 <133433930+jpartida10@users.noreply.github.com>
2025-11-08 14:27:36 -0600

    Update database connection URL for NeonDB

--- db_conexion.py
+++ db_conexion.py
@@ -1,7 +1,7 @@
 import sqlalchemy
 
 # URL que Render te proporciona
-DATABASE_URL = "postgresql://asistencia_db_c026_user:Tj2lei1PKAv1jmrAnplSMBRFldW3FHhk@dpg-d47pamshg0os73frp270-a/asistencia_db_c026"
+DATABASE_URL = "postgresql://neondb_owner:npg_1f3sluIdFRyA@ep-solitary-meadow-adthlkqa-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 def get_connection():
     engine = sqlalchemy.create_engine(DATABASE_URL)
     return engine.connect()
